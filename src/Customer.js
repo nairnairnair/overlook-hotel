1cbe9056016c79211b2c381e13dc80f085f00f59
emmacintosh <emmammrussell@gmail.com>
2022-08-06 15:23:41 -0600

    Add tests and methods to Customer class to check money spent and existing bookings

--- src/Customer.js
+++ src/Customer.js
@@ -1,7 +1,42 @@
+// import Booking from './Booking';
+// import Room from './Room';
+
 class Customer {
     constructor(customerData){
         this.id = customerData.id;
         this.name = customerData.name;
+        this.bookings = [];
+        this.bookedRooms = [];
+        // this.totalMoneySpent
+    }
+
+    // getNewCustomerBooking(room){
+    //     let newRoom = new Room(room)
+    //     this.bookings.push(newRoom)
+    //     this.totalMoneySpent += room.costPerNight
+    // }
+
+    checkExistingBookings(bookings){
+      let filteredBookings = bookings.bookings.filter((booking)=>{
+          return this.id === booking.userID
+        })
+        filteredBookings.forEach((booking) => {
+            this.bookings.push(booking)
+        })
+        return this.bookings
+    }
+
+    checkTotalMoneySpent(rooms){
+        let roomNumbers = this.bookings.map((booking)=> booking.roomNumber)
+        rooms.rooms.forEach((room)=>{
+            if (roomNumbers.includes(room.number)){
+                this.bookedRooms.push(room)
+            }
+        })
+        let totalCost = this.bookedRooms.reduce((acc, room) => {
+            return acc += room.costPerNight
+        }, 0)
+          return totalCost
     }
 }
 
--- test/Customer-test.js
+++ test/Customer-test.js
@@ -5,6 +5,8 @@ let customerData1;
 let customer1;
 let customerData2;
 let customer2;
+let bookings;
+let rooms;
 
 describe('Customer', () => {
 
@@ -13,6 +15,56 @@ describe('Customer', () => {
         customer1 = new Customer(customerData1)
         customerData2 = { "id": 51, "name": "Puguberto Schmitz" }
         customer2 = new Customer(customerData2)
+        bookings = {
+            "bookings": [
+                {
+                "id": "5fwrgu4i7k55hl6sz",
+                "userID": 9,
+                "date": "2022/04/22",
+                "roomNumber": 15
+                },
+                {
+                "id": "5fwrgu4i7k55hl6t5",
+                "userID": 43,
+                "date": "2022/01/24",
+                "roomNumber": 24
+                },
+                {
+                "id": "5fwrgu4i7k55hl6t6",
+                "userID": 13,
+                "date": "2022/01/10",
+                "roomNumber": 12
+            }
+        ]
+    }
+        rooms = {
+            "rooms": [
+                {
+                "number": 1,
+                "roomType": "residential suite",
+                "bidet": true,
+                "bedSize": "queen",
+                "numBeds": 1,
+                "costPerNight": 358.4
+                },
+                {
+                "number": 2,
+                "roomType": "suite",
+                "bidet": false,
+                "bedSize": "full",
+                "numBeds": 2,
+                "costPerNight": 477.38
+                },
+                {
+                "number": 15,
+                "roomType": "residential suite",
+                "bidet": false,
+                "bedSize": "full",
+                "numBeds": 1,
+                "costPerNight": 294.56
+                }
+            ]
+        }
     })
 
     it('should be a function', () => {
@@ -31,5 +83,22 @@ describe('Customer', () => {
     it('should have an id', () => {
         expect(customer1.id).to.equal(9)
         expect(customer2.id).to.equal(51)
+    })
+
+    it('should be able to check existing bookings', () => {
+        customer1.checkExistingBookings(bookings)
+        expect(customer1.bookings).to.deep.equal([
+            {
+            "id": "5fwrgu4i7k55hl6sz",
+            "userID": 9,
+            "date": "2022/04/22",
+            "roomNumber": 15
+            }
+        ])
+    })
+
+    it('should be able to check total money spent', () => {
+        customer1.checkExistingBookings(bookings)
+        expect(customer1.checkTotalMoneySpent(rooms)).to.equal(294.56)
     })
 })
